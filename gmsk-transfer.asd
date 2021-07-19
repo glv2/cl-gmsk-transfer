6cc1389c6550c1bbfeaa4053628e271783d746e5
Guillaume Le Vaillant <glv@posteo.net>
2021-07-19 22:50:11 +0200

    Add tests

--- gmsk-transfer.asd
+++ gmsk-transfer.asd
@@ -10,4 +10,19 @@
   :author "Guillaume LE VAILLANT"
   :license "GPL-3"
   :depends-on ("cffi")
+  :in-order-to ((test-op (test-op "gmsk-transfer/tests")))
   :components ((:file "gmsk-transfer")))
+
+(defsystem "gmsk-transfer/tests"
+  :name "gmsk-transfer/tests"
+  :description "Tests fot gmsk-transfer"
+  :version "1.0"
+  :author "Guillaume LE VAILLANT"
+  :license "GPL-3"
+  :depends-on ("fiveam" "gmsk-transfer" "uiop")
+  :in-order-to ((test-op (load-op "gmsk-transfer/tests")))
+  :perform (test-op (o s)
+             (let ((tests (uiop:find-symbol* 'gmsk-transfer-tests
+                                             :gmsk-transfer-tests)))
+               (uiop:symbol-call :fiveam 'run! tests)))
+  :components ((:file "tests")))
---
+++ tests.lisp
@@ -0,0 +1,51 @@
+;;; This file is part of cl-gmsk-transfer
+;;; Copyright 2021 Guillaume LE VAILLANT
+;;; Distributed under the GNU GPL v3 or later.
+;;; See the file LICENSE for terms of use and distribution.
+
+(defpackage :gmsk-transfer-tests
+  (:use :cl :fiveam :gmsk-transfer)
+  (:import-from :uiop
+                #:with-temporary-file))
+
+(in-package :gmsk-transfer-tests)
+
+
+(def-suite gmsk-transfer-tests :description "Tests for gmsk-transfer.")
+(in-suite gmsk-transfer-tests)
+
+(defparameter *message*
+  (asdf:system-relative-pathname "gmsk-transfer" "LICENSE"))
+
+(defun same-files-p (path-1 path-2)
+  (with-open-file (file-1 path-1 :element-type '(unsigned-byte 8))
+    (with-open-file (file-2 path-2 :element-type '(unsigned-byte 8))
+      (let ((buffer-1 (make-array 1024 :element-type '(unsigned-byte 8)))
+            (buffer-2 (make-array 1024 :element-type '(unsigned-byte 8))))
+        (labels ((same-p ()
+                   (let ((i (read-sequence buffer-1 file-1))
+                         (j (read-sequence buffer-2 file-2)))
+                     (cond
+                       ((not (= i j)) nil)
+                       ((zerop i) t)
+                       (t (same-p))))))
+          (same-p))))))
+
+(test transmit-and-receive-file
+  (with-temporary-file (:pathname samples)
+    (with-temporary-file (:pathname decoded)
+      (let ((radio (format nil "file=~a" (namestring samples))))
+        (transmit-file (namestring *message*) :radio-driver radio)
+        (receive-file (namestring decoded) :radio-driver radio)
+        (is (same-files-p *message* decoded))))))
+
+(test transmit-and-receive-stream
+  (with-temporary-file (:pathname samples)
+    (with-temporary-file (:pathname decoded :stream decoded-stream)
+      (with-open-file (message-stream *message*
+                                      :element-type '(unsigned-byte 8))
+        (let ((radio (format nil "file=~a" (namestring samples))))
+          (transmit-stream message-stream :radio-driver radio)
+          (receive-stream decoded-stream :radio-driver radio)))
+      :close-stream
+      (is (same-files-p *message* decoded)))))
